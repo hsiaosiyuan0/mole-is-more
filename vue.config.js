2af2c0a1450816938a4cc6f267224e29953e10f7
hsiaosiyuan0 <hsiaosiyuan0@gmail.com>
2021-11-25 22:15:35 +0800

    feat: update

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "doc"),
+  outputDir: path.resolve(__dirname, "docs"),
   publicPath: process.env.NODE_ENV == "production" ? "/mole-is-more" : "/",
   chainWebpack: (config) => {
     config.resolve.symlinks(false);
